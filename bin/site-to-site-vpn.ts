f946b969a0a27f33e096574c826b51762836fd40
Martin <mart256@gmail.com>
2022-05-30 22:49:56 +0200

    Added new stack to the bin

--- bin/site-to-site-vpn.ts
+++ bin/site-to-site-vpn.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import { OnPremisesStack } from '../lib/on-premises-stack';
 import { SiteToSiteVpnStack } from '../lib/site-to-site-vpn-stack';
 
 const app = new cdk.App();
 new SiteToSiteVpnStack(app, 'SiteToSiteVpnStack');
+new OnPremisesStack(app, 'OnPremisesVpnStack');
